e7e9ea7eb3ddf5074ba48df78fc9741f10d47530
Hasan184990 <kucinghasan82@gmail.com>
2025-02-20 17:06:21 +0700

    praktikum 1

---
+++ variabel_user.php
@@ -0,0 +1,13 @@
+<?php
+// mendefinisikan variabel
+$nama = 'Hasan';
+$umur = 21;
+$berat = 60.20;
+
+echo 'nama saya ' . $nama;
+echo '<br/>Umur :' . $umur;
+echo '<br/>Berat :' . $berat;  
+
+
+
+?>
